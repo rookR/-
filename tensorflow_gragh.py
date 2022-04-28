6ef749baf8ebd70cf8f6efa5a1d201ab64fbf973
rookR <63527173+rookR@users.noreply.github.com>
2022-04-28 12:44:51 +0800

    Update and rename tensor_list.py to tensorflow_gragh.py

--- tensorflow_gragh.py
+++ tensorflow_gragh.py
@@ -8,6 +8,19 @@ with tf.gfile.FastGFile(os.path.join(detection_pb), 'rb') as f:
     tf.import_graph_def(graph_def, name='')
 
 
+# pb模型文件节点信息保存到log文件夹内，用tensorboard查看
+with tf.Session() as sess:
+    with tf.gfile.GFile(os.path.join(detection_pb), 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        g_in = tf.import_graph_def(graph_def)
+        train_writer = tf.summary.FileWriter("./log")
+        train_writer.add_graph(sess.graph)
+        train_writer.flush()
+        train_writer.close()
+    
+    
+    
 tensor_name_list = [tensor.name for tensor in tf.get_default_graph().as_graph_def().node]
 for tensor_name in tensor_name_list:
     print(tensor_name,'\n')
